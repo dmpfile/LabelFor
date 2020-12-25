ced96f6b1c56049f792473c21d548090ea62b115
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2020-12-26 01:00:51 +0900

    [update]EditNav.tsx：ナビをクリックしたらカーソルの形を変える

--- src/components/molecules/EditNav.tsx
+++ src/components/molecules/EditNav.tsx
@@ -4,12 +4,17 @@ import { ReactComponent as Text} from '../../assets/text.svg';
 import { ReactComponent as Image} from '../../assets/image.svg';
 
 const EditNav: React.FC = () => {
+  const changeCursor = (e: React.MouseEvent<HTMLElement>) => {
+    const type = e.currentTarget.dataset.item;
+    document.body.style.cursor = type === 'text' ? 'text': '';
+  }
+
   return (
     <div className="editnav">
       <div className="editnav__inner">
         <ul>
-          <li>テキスト<div className="editnav__logo"><Text/></div></li>
-          <li>画像<div className="editnav__logo"><Image/></div></li>
+          <li>テキスト<div className="editnav__logo" onClick={changeCursor} data-item={'text'}><Text/></div></li>
+          <li>画像<div className="editnav__logo" onClick={changeCursor}><Image/></div></li>
         </ul>
       </div>
     </div>
