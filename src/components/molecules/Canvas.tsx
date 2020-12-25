46180bc622eb70a3ecfbe2807004a9a1600bcd49
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2020-12-26 01:02:37 +0900

    [update]Canvas.tsx：テキストカーソルでキャンバスをクリックしたら要素を追加する処理実装

--- src/components/molecules/Canvas.tsx
+++ src/components/molecules/Canvas.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../scss/canvas.scss';
 
 const Canvas: React.FC = () => {
   /* 動的にキャンバスサイズを変える */
   const canvasSize = { width: '50vh', height: '50vh' };
+
+  const [texts, setText] = useState<any>([])
+
+  const addTextElements = (e: React.MouseEvent<HTMLElement>) => {
+    const cursor = document.body.style.cursor;
+    const left = `${String(e.nativeEvent.offsetX)}px`
+    const top = `${String(e.nativeEvent.offsetY)}px`
+    if (cursor === 'text') {
+      setText([
+        ...texts,
+        {
+          text: 'テキストを入力',
+          top,
+          left
+        }
+      ])
+    }
+  };
   return (
-    <div className="canvas" style={canvasSize}>
+    <div className="canvas" style={canvasSize} onClick={addTextElements}>
       <div className="canvas__inner">
         {/* テキスト・画像を追加するとここに要素が追加 */}
+        {texts.map((text: any, index: number) => (
+          <div className={`canvas_text${String(index + 1)}`} key={index} style={{top: text.top, left: text.left}}>{text.text}</div>
+        ))}
       </div>
     </div>
   )
