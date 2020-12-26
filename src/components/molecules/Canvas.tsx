fd734af2e62d8b6b91274c5009f2630ac4cc703d
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2020-12-26 22:38:19 +0900

    [update]Canvas.tsx：テキスト編集の方法変更

--- src/components/molecules/Canvas.tsx
+++ src/components/molecules/Canvas.tsx
@@ -8,13 +8,9 @@ const Canvas: React.FC = () => {
   const [texts, setText] = useState<any>([])
 
   const addTextElements = (e: React.MouseEvent<HTMLElement>) => {
-    if (document.getElementById('canvas_input')?.childNodes.length) {
-      ReactDOM.unmountComponentAtNode(document.getElementById('canvas_input')!);
-      return true;
-    }
-
     const el = e.target as HTMLInputElement;
     const cursor = document.body.style.cursor;
+    
     if (cursor === 'text' && el.className === 'canvas__inner') {
       const left = `${String(e.nativeEvent.offsetX)}px`
       const top = `${String(e.nativeEvent.offsetY)}px`
@@ -30,49 +26,29 @@ const Canvas: React.FC = () => {
     } else if (cursor === 'text') {
       e.currentTarget.childNodes.forEach((i) => {
         const t = i as HTMLElement
+        if (t.className === el.className) {
+          return false;
+        } 
         t.className = t.className.replace(' is-active', '')
       })
       
-      if(!el.classList.contains('is-active')) {
-        if (el.type !== 'text') {
-          el.className += ' is-active'
-          addInputElements(el)
-        }
+      if(!el.classList.contains('is-active') && el.type !== 'text') {
+        el.className += ' is-active'
+      }
+      
+      if (el.type !== 'text'){
+        el.contentEditable = 'true'
+        el.focus()
       }
     }
   };
-
-  const addInputElements = (target: HTMLInputElement) => {
-    const currentText = target.innerHTML
-    const targetPosition = {top: `${target.style.top}`, left: `${target.style.left}`, position: 'absolute'} as React.CSSProperties
-    ReactDOM.render(
-      <input
-        type="text"
-        defaultValue={currentText}
-        onKeyPress={editTextElements}
-        style={targetPosition}
-        data-item={target.className} 
-        autoFocus>
-      </input>,
-      document.getElementById('canvas_input')
-    )
-  }
-
-  const editTextElements = (e: any) => {
-    if (e.key === 'Enter') {
-      const id = Number(e.target.dataset.item.replace('canvas_text', '').replace('is-active', ''))
-      texts[id - 1].text = e.target.value
-      setText([...texts])
-      ReactDOM.unmountComponentAtNode(document.getElementById('canvas_input')!);
-    }
-  }
 
   return (
     <div className="canvas" style={canvasSize}>
       <div className="canvas__inner" onClick={addTextElements}>
         {/* テキスト・画像を追加するとここに要素が追加 */}
         {texts.map((text: any, index: number) => (
-          <div 
+          <div
             className={`canvas_text${String(index + 1)}`}
             key={index}
             style={{top: text.top, left: text.left}}
@@ -80,7 +56,6 @@ const Canvas: React.FC = () => {
             {text.text}
           </div>
         ))}
-        <div id="canvas_input" className="canvas_input"></div>
       </div>
     </div>
   )
