706a397ded458e82fc9702218e843692476618c6
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2021-01-23 20:51:36 +0900

    feat:header.tsx:gitとtwitterアイコン、リンク追加

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../../scss/header.scss';
+import { ReactComponent as Github} from '../../assets/github.svg';
+import { ReactComponent as Twitter} from '../../assets/twitter.svg';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header__inner">
         <h1 className="header__title">Label for</h1>
+        <div className="header__logo">
+          <a href='https://github.com/dmpfile' target="blank" rel="noopener"><Github /></a>
+          <a href='https://twitter.com/dmpfile' target="blank" rel="noopener"><Twitter /></a>
+        </div>
       </div>
     </header>
   )
