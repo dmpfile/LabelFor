a4c7cb2eee34278ce180c3d1c3af89bab0d76573
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2021-02-28 01:45:39 +0900

    feat: 画像ダウンロード機能追加

--- src/components/molecules/EditNav.tsx
+++ src/components/molecules/EditNav.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import '../../scss/editnav.scss';
 import { ReactComponent as Text} from '../../assets/text.svg';
-//import { ReactComponent as Image} from '../../assets/image.svg';
+import { ReactComponent as Image} from '../../assets/image.svg';
 import { ReactComponent as Printer} from '../../assets/printer.svg';
+import html2canvas from 'html2canvas';
 
 const EditNav: React.FC = () => {
   const changeCursor = (e: React.MouseEvent<HTMLElement>) => {
     const type = e.currentTarget.dataset.item;
     document.body.style.cursor = type === 'text' ? 'text': '';
+  }
+
+  const downloadImg = () => {
+    html2canvas(document.querySelector('.canvas__inner') as HTMLElement).then(canvas => {
+      const a = document.createElement('a')
+      a.href = canvas.toDataURL("image/png")
+      a.download = "canvas.png"
+      a.click()
+      a.remove()
+    })
   }
 
   const printCanvas = () => {
@@ -19,7 +30,7 @@ const EditNav: React.FC = () => {
       <div className="editnav__inner">
         <ul>
           <li>テキスト<div className="editnav__logo" onClick={changeCursor} data-item={'text'}><Text/></div></li>
-          {/* <li>画像<div className="editnav__logo" onClick={changeCursor}><Image/></div></li> */}
+          <li>保存<div className="editnav__logo" onClick={downloadImg}><Image/></div></li>
           <li>印刷<div className="editnav__logo" onClick={printCanvas}><Printer/></div></li>
         </ul>
       </div>
